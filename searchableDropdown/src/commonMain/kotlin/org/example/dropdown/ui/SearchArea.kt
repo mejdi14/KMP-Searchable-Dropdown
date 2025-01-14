d8b0993bcbd67ee9e6ceb1ac502927360413912b
mejdi14 <mejdiclubiste@gmail.com>
2025-01-14 23:36:25 +0100

    separate icon and input composables

--- searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/SearchArea.kt
+++ searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/SearchArea.kt
@@ -3,8 +3,6 @@ package org.example.dropdown.ui
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
@@ -14,9 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import kmp_searchable_dropdown.searchabledropdown.generated.resources.Res
-import kmp_searchable_dropdown.searchabledropdown.generated.resources.search_icon
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun SearchArea(searchQuery: MutableState<String>) {
@@ -25,25 +20,12 @@ fun SearchArea(searchQuery: MutableState<String>) {
         verticalAlignment = Alignment.CenterVertically
     ) {
 
-        Icon(
-            painterResource(Res.drawable.search_icon), "",
-            modifier = Modifier.width(50.dp)
-        )
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(56.dp),
-            value = searchQuery.value,
-            onValueChange = { searchQuery.value = it },
-            placeholder = { Text("Searching...") },
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent
-            )
-        )
+        SearchIconComposable()
+        SearchInputComposable(searchQuery)
 
     }
 
 }
+
+
+
---
+++ searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/SearchInputComposable.kt
@@ -0,0 +1,30 @@
+package org.example.dropdown.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun SearchInputComposable(searchQuery: MutableState<String>) {
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        value = searchQuery.value,
+        onValueChange = { searchQuery.value = it },
+        placeholder = { Text("Searching...") },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
