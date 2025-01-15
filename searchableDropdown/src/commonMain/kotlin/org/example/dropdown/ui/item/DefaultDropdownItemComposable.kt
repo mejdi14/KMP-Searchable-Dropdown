e9e3941a796798050a43051f8099cece09f6be0f
mejdi14 <mejdiclubiste@gmail.com>
2025-01-15 16:34:09 +0100

    save progress

--- searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/item/DefaultDropdownItemComposable.kt
+++ searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/item/DefaultDropdownItemComposable.kt
@@ -1,8 +1,12 @@
 package org.example.dropdown.ui.item
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 
 @Composable
 internal fun DefaultDropdownItemComposable() {
+    Row(verticalAlignment = Alignment.CenterVertically){
 
+    }
 }
