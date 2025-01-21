d130a0885b6ebfe77cbe000a15133877f92df871
mejdi14 <mejdiclubiste@gmail.com>
2025-01-21 09:29:37 +0100

    add more options to the text

--- searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/item/DefaultItemHeaderComposable.kt
+++ searchableDropdown/src/commonMain/kotlin/org/example/dropdown/ui/item/DefaultItemHeaderComposable.kt
@@ -3,6 +3,7 @@ package org.example.dropdown.ui.item
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kmp_searchable_dropdown.searchabledropdown.generated.resources.Res
 import kmp_searchable_dropdown.searchabledropdown.generated.resources.cross_icon
@@ -29,8 +31,11 @@ import kotlin.reflect.KProperty1
 fun <T> DefaultItemHeaderComposable(
     backgroundColor: Color = Color(0xFFF5F6F8),
     shape: Shape = RoundedCornerShape(4.dp),
+    paddingValues: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 2.dp),
     deleteIcon: DrawableResource = Res.drawable.cross_icon,
     iconColor: Color = Color.Black,
+    textColor: Color = iconColor,
+    spaceBetween: Dp = 5.dp,
     item: T,
     title: KProperty1<T, *>,
     removeItemListener: MultipleRemoveItemListener<T>
@@ -38,10 +43,10 @@ fun <T> DefaultItemHeaderComposable(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.background(color = backgroundColor, shape = shape)
-            .padding(horizontal = 8.dp, vertical = 2.dp)
+            .padding(paddingValues)
     ) {
-        Text(title.get(item).toString())
-        Spacer(Modifier.width(5.dp))
+        Text(title.get(item).toString(), color = textColor)
+        Spacer(Modifier.width(spaceBetween))
         Icon(
             painter = painterResource(deleteIcon), contentDescription = "",
             tint = iconColor,
